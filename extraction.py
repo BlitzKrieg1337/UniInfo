855c322631984e952b7f39f2b603f0d748c2dd8a
Your Name <dhanishsoni1@gmail.com>
2026-08-18 00:27:54 +0530

    Updated Data Extraction

--- extraction.py
+++ extraction.py
@@ -1,21 +1,31 @@
+import os
+from pathlib import Path 
 from docling.document_converter import DocumentConverter
+
+os.environ["TORCHDYNAMO_DISABLE"] = "1"
 
 class DocumentExtractor:
 
-    def get_url_text(self, url:str, file_name:str):
+    def extract(self, source:str, file_name:str, folder:str):
         converter = DocumentConverter()
         try: 
-            result = converter.convert(url).document
+            source = source.strip(' "\'')
+            output_dir = Path(__file__).parent / "data" / folder
+            output_dir.mkdir(parents=True, exist_ok=True)
+
+            result = converter.convert(source).document
             doc = result.export_to_markdown()
 
-            with open(f"data/URL/{file_name}.md", "w", encoding = "utf-8") as f:
+            with open(f"data/{folder}/{file_name}.md", "w", encoding = "utf-8") as f:
                 f.write(doc)
-            print("Saved URL.")
+            print(f"Saved: data/{folder}/{file_name}.md")
         except Exception as e:
             print(f"Extraction failed: {e}")
 
 #Add Prgoram URLs with file names(TCD, UCD, DCU)
 obj = DocumentExtractor()
-url = input("Please enter URl: ")
-filename = input("Please enter file name: ")
-obj.get_url_text(url, filename)
+
+source = input("Please enter source: ")
+file_name = input("Please enter file name: ")
+folder_name = input("Enter folder name: ")
+obj.extract(source, file_name, folder_name)
