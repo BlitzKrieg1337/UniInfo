048191d5e796a17655498b942d1ab0c761b1931b
Your Name <dhanishsoni1@gmail.com>
2026-08-17 22:08:09 +0530

    Added Data Extraction

--- extraction.py
+++ extraction.py
@@ -4,12 +4,15 @@ class DocumentExtractor:
 
     def get_url_text(self, url:str, file_name:str):
         converter = DocumentConverter()
-        result = converter.convert(url).document
-        doc = result.export_to_markdown()
+        try: 
+            result = converter.convert(url).document
+            doc = result.export_to_markdown()
 
-        with open(f"data/URL/{file_name}.md", "w", encoding = "utf-8") as f:
-            f.write(doc)
-        print("Saved URL.")
+            with open(f"data/URL/{file_name}.md", "w", encoding = "utf-8") as f:
+                f.write(doc)
+            print("Saved URL.")
+        except Exception as e:
+            print(f"Extraction failed: {e}")
 
 #Add Prgoram URLs with file names(TCD, UCD, DCU)
 obj = DocumentExtractor()
